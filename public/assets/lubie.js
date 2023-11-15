1548d5c29397b4074accad8833b813795c7bf18b
Andre Dan Dayaganon <your-email@example.com>
2023-11-15 16:49:41 +0800

    herosection added

--- public/assets/lubie.js
+++ public/assets/lubie.js
@@ -1,4 +1,5 @@
 import Lubiehero from "../assets/lubiehero.png";
+import LubieHero2 from "../assets/lubieheader.png"
 import icon1 from "../assets/lubieIcon1.svg"
 import icon2 from "../assets/lubieIcon2.svg"
 import icon3 from "../assets/lubieIcon3.svg"
@@ -8,6 +9,7 @@ import icon6 from "../assets/lubieIcon6.svg"
 
 export {
     Lubiehero,
+    LubieHero2,
     icon1,
     icon2,
     icon3,
