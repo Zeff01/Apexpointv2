ef3e597b1e353416ac98d41f8d8469a7bbe590e8
adeldiola <adelainediola@gmail.com>
2023-11-15 15:43:27 +0300

    added product section

--- app/Pages/lubie/page.tsx
+++ app/Pages/lubie/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Herosec from './section/Herosec'
+import Products from './section/Products'
 export default function page() {
   return (
     <div className="border-2 border-black flex-col text-black bg-white w-full h-screen">
         <div className=""><Herosec/></div>
+        <div className=""><Products /></div>
 
         </div>
 
