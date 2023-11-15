33ddf88b2d7237676fd66d62280dd3cf4ea38820
adeldiola <adelainediola@gmail.com>
2023-11-15 08:45:56 +0300

    added sections > Page\lubie

--- app/Pages/lubie/page.tsx
+++ app/Pages/lubie/page.tsx
@@ -3,7 +3,19 @@ import React from 'react'
 export default function page() {
   return (
     <div className="border-2 border-black flex-col bg-white w-full h-screen">
-        <div className=""></div>
+       
+       <section>
+        <div>Navbar & Menu Bar</div>
+        Hero Section
+       </section>
+       <section>Product Section</section>
+       <section>About Section</section>
+       <section>Testimonial Section</section>
+       <section>Contact Section</section>
+       <section>Footer Section</section>
+
+       
+        <div className=""> Hero </div>
 
     </div>
   )
