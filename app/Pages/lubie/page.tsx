0329c426e50eff2a0d961f6bd66992a74de6e635
Andre Dan Dayaganon <your-email@example.com>
2023-11-15 18:34:30 +0800

    lubie page

--- app/Pages/lubie/page.tsx
+++ app/Pages/lubie/page.tsx
@@ -2,11 +2,7 @@ import React from 'react'
 import Herosec from './section/Herosec'
 export default function page() {
   return (
-<<<<<<< Updated upstream
-    <div className="border-2  flex-col text-black bg-white w-full h-screen">
-=======
     <div className="border-2 border-black flex-col text-black bg-white w-full h-screen">
->>>>>>> Stashed changes
         <div className=""><Herosec/></div>
 
     </div>
