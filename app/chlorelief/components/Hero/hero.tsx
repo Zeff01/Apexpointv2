b51a4e8ff889af3e0f19f351e6047e2d884f6d30
JUSTINEYUNG02 <justineyung02@gmail.com>
2023-11-16 19:56:13 +0800

    hero

--- app/chlorelief/components/Hero/hero.tsx
+++ app/chlorelief/components/Hero/hero.tsx
@@ -7,7 +7,22 @@
 export default function Hero(){
 
     return(
-        <h1>Hero</h1>
+        <div className="bg-green-600 h-64 flex items-center justify-center">
+            <div className="flex items-center">
+                <img src="/assets/baby.png" 
+                alt="babypic"
+                width={250}
+                height={30}/>
+                <h2 className="text-2xl font-bold">YOUR FIRST LINE OF DEFENSE</h2>
+                <img src="/assets/all.png" 
+                alt="all"
+                width={250}
+                height={30}/>
+                <h2>Relieves itch and Rash</h2>
+            </div>
+        </div>
+        
+        
     )
 
 }
