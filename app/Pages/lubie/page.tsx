c4f812d4cd604253ff4c505a8aeba2f530965bed
Ivor <lhanivor@gmail.com>
2023-11-16 11:27:26 +0800

    fix conflicts

--- app/Pages/lubie/page.tsx
+++ app/Pages/lubie/page.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import Navbar from './section/Navbar'
 
+import React from 'react'
+import Herosec from './section/Herosec'
 export default function page() {
   return (
-    <div>
-      <Navbar/>
-    </div>
+    <div className="border-2 border-black flex-col text-black bg-white w-full h-screen">
+        <div className=""><Herosec/></div>
+
+        </div>
+
+      
   )
 }
