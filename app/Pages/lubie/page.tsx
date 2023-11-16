aca6124731b24172fcc7d6136ff1080bb2de44e2
Andre Dan Dayaganon <your-email@example.com>
2023-11-16 22:39:54 +0800

    AP-15

--- app/Pages/lubie/page.tsx
+++ app/Pages/lubie/page.tsx
@@ -3,7 +3,7 @@ import Herosec from './section/Herosec'
 import Navbar from './section/Navbar'
 import Contact from './section/Contact'
 import Products from './section/products'
-
+// layout
 export default function Page() {
   return (
     <div className="border-2 border-black flex-col text-black bg-white w-full h-screen">
