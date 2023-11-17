890832632cedd16f6bba87a9fc9802e89720edbf
Ivor <lhanivor@gmail.com>
2023-11-17 23:18:22 +0800

    responsive testionials

--- app/Pages/lubie/components/cardslide.tsx
+++ app/Pages/lubie/components/cardslide.tsx
@@ -1,76 +1,30 @@
-import React, { useState, useEffect } from "react";
-import PropTypes from "prop-types";
-import { ChevronLeft, ChevronRight } from "react-feather";
-import Cards from "./cards";
+// Cardslide.jsx
+import React from 'react';
+import { Pagination, Scrollbar, A11y, Autoplay } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import Cards from './cards'; // Import your Cards component
+import 'swiper/css'; // Import Swiper styles
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
-function Carousel({ autoSlide = true, autoSlideInterval = 3000 }) {
-    const [curr, setCurr] = useState(0);
-  
-    const slides = [
-      <Cards key={1}/>,
-      // Add more JSX elements for additional slides
-    ];
-    
-  
-    const prev = () =>
-      setCurr((curr) => (curr === 0 ? slides.length - 1 : curr - 1));
-  
-    const next = () => setCurr((curr) => (curr === slides.length - 1 ? 0 : curr + 1));
-  
-    useEffect(() => {
-      if (!autoSlide) return;
-  
-      const slideInterval = setInterval(() => {
-        setCurr((prevCurr) => (prevCurr === slides.length - 1 ? 0 : prevCurr + 1));
-      }, autoSlideInterval);
-  
-      return () => clearInterval(slideInterval);
-    }, [autoSlide, autoSlideInterval]);
-  
-    return (
-      <div className="overflow-hidden relative">
-        <div
-          className="flex transition-transform ease-out duration-500"
-          style={{ transform: `translateX(-${curr * 100}%)` }}
-        >
-          {slides}
-        </div>
-        <div className="absolute inset-0 flex items-center justify-between p-4">
-          <button
-            onClick={prev}
-            className="p-1 rounded-full shadow bg-white/80 text-gray-800 hover:bg-white"
-          >
-            <ChevronLeft size={40} />
-          </button>
-          <button
-            onClick={next}
-            className="p-1 rounded-full shadow bg-white/80 text-gray-800 hover:bg-white"
-          >
-            <ChevronRight size={40} />
-          </button>
-        </div>
-  
-        <div className="absolute bottom-4 right-0 left-0">
-          <div className="flex items-center justify-center gap-2">
-            {slides.map((_, i) => (
-              <div
-                key={i}
-                className={`
-                  transition-all w-3 h-3 bg-white rounded-full
-                  ${curr === i ? "p-2" : "bg-opacity-50"}
-                `}
-              />
-            ))}
-          </div>
-        </div>
-      </div>
-    );
-  }
-  
-  Carousel.propTypes = {
-    autoSlide: PropTypes.bool,
-    autoSlideInterval: PropTypes.number,
-  };
-  
-
-export default Carousel;
+export default function Cardslide() {
+  return (
+    <Swiper
+    modules={[Pagination, Scrollbar, A11y, Autoplay]}
+    spaceBetween={50} // Adjust space between slides
+    slidesPerView={1} // Show one card at a time
+    pagination={{ clickable: true }} // Enable clickable pagination
+    scrollbar={{ draggable: true }} // Enable draggable scrollbar
+    autoplay={{
+      delay: 2500, // Delay in milliseconds between slides
+      disableOnInteraction: false // Continue autoplay after interaction
+    }}
+  >
+    <SwiperSlide><Cards /></SwiperSlide>
+    <SwiperSlide><Cards /></SwiperSlide>
+    <SwiperSlide><Cards /></SwiperSlide>
+    {/* Add more SwiperSlides if needed */}
+  </Swiper>
+  );
+}
