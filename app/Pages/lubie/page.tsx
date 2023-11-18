0250bec14a36f68b962490ed7918e798a8b73c93
ErickaConcepcion <eriorion001@gmail.com>
2023-11-19 02:10:58 +0800

    Added About Us Section

--- app/Pages/lubie/page.tsx
+++ app/Pages/lubie/page.tsx
@@ -4,15 +4,25 @@ import Navbar from './section/Navbar'
 import StoreSection from './section/Store'
 import Products from './section/products'
 import FooterSection from './section/Footersec'
+<<<<<<< Updated upstream
 import Testimonials from './section/Testimonials'
+=======
+import AboutSection from './section/AboutUs'
+>>>>>>> Stashed changes
 
 // layout
 export default function Page() {
   return (
+<<<<<<< Updated upstream
     <div className="flex-col bg-white w-full h-screen">
 
+=======
+    <div className=" bg-white w-full h-screen">
+        <div className="">
+>>>>>>> Stashed changes
           <Navbar/>
-          <Herosec/>
+          {/* <Herosec/> */}
+          <AboutSection/>
           <StoreSection/>
           <Testimonials/>
           <FooterSection/>
