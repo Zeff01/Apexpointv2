a19663fcba0fef97f6369ffc45388e36652c28e1
Ivor <lhanivor@gmail.com>
2023-11-18 08:21:47 +0800

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -76,29 +76,26 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      // screens: {
-      //   'sm': {'max': '640px'},    // Mobile screens
-      //   'md': {'max': '767px'},    // Tablet screens
-      //   'lg': {'max': '1023px'},   // Small desktop screens
-      //   'xl': {'max': '1279px'},   // Large desktop screens
-      // },
-      colors: {
-        lubie: {
-          'white-color': "#FFFFFF",
-          'light-blue': "#B6CBE0",
-          'navy-blue': "#1C3079",
-          'gray-white': '#F0F5F3',
-          'green-light': "#7FF0C3",
-        },
+      fontFamily: {
+        'lubie' : ['Ubuntu', 'Open Sans'],
+       },
+        colors: {
+          lubie: {
+        	'dark-blue': '#0A1950',
+            'white' : '#FFFFFF',
+            'sky-blue': '#B6CBE0',
+             'gray-white' : '#F0F5F3',
+             'green-light': '#7FF0C3',
+          },
         apexpoint: {},
         chlorelief: {},
       },
     },
   },
 
-  
 
-  
+
+
   plugins: [],
 >>>>>>> 84de1f1 (rebase)
 };
