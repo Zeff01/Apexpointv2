2427505014ceb8be1e30ac06bd909009b8d8bfaf
JUSTINEYUNG02 <justineyung02@gmail.com>
2023-11-18 17:47:57 +0800

    Added for products

--- app/chlorelief/components/Hero/hero.tsx
+++ app/chlorelief/components/Hero/hero.tsx
@@ -7,18 +7,7 @@
 export default function Hero(){
 
     return(
-        // <div className="bg-green-600 h-64 flex items-center justify-center">
-        //     <div className="flex items-center">
-        //         <img src="/assets/baby.png" 
-        //         alt="babypic"
-        //         style={{ maxWidth: '100%', marginBottom: '10px' }} />
-        //         <h2 className="text-2xl font-bold">YOUR FIRST LINE OF DEFENSE</h2>
-        //         <img src="/assets/all.png" 
-        //         alt="all"
-        //         style={{ maxWidth: '100%', marginBottom: '10px' }} />
-        //         <h2>Relieves itch and Rash</h2>
-        //     </div>
-        // </div>
+        
         <div className="bg-green-600 flex items-center justify-center flex-wrap">
             <img
                 src="/assets/baby.png"
@@ -37,23 +26,7 @@ export default function Hero(){
             
         </div>
 
-        // <div className="bg-green-600 flex flex-col md:flex-row items-center justify-center">
-        //     <img
-        //         src="/assets/baby.png"
-        //         className="mx-auto md:ml-0 md:mr-4 mb-4 md:mb-0"
-        //         alt="babypic"
-        //         style={{ maxWidth: '100%' }}
-        //     />
-        //     <div className="text-center">
-        //         <h2 className="text-2xl font-bold text-blue-500 mb-4 md:mb-0">YOUR FIRST LINE OF DEFENSE</h2>
-        //         <img
-        //             src="/assets/all.png"
-        //             alt="all"
-        //             className="max-w-full mb-4"
-        //         />
-        //         <h2 className="text-green-800">Relieves itch and Rash</h2>
-        //     </div>
-        // </div>
+        
 
     )
 
