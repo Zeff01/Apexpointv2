bbd8a45c40c5168cbe140416bfb2c9bca5f9e335
420ku <mordecai51213@gmail.com>
2023-11-20 16:27:56 +0800

    added section folder, finalized folder structure, merged components for team pull

--- app/chlorelief/page.tsx
+++ app/chlorelief/page.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
+import LandingPage from './section/LandingPage';
 
-import Contacts from './components/Contacts'
-import Hero from './components/Hero/hero'
-import Product from './components/Product/product'
+
 export default function page() {
   return (
-    <>
     
+    <LandingPage />
     
-    <Hero />
-    <Product />
-    <Contacts />
-    </>
+   
+    
   )
 }
---
+++ app/chlorelief/section/LandingPage.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
+import Hero from '../components/Hero/hero'
+import Product from '../components/Product/product'
+import Contacts from '../components/Contacts/Contacts'
+import Footer from '../components/Footer/Footer'
+
+export default function LandingPage() {
+  return (
+    <>
+    <Navbar />
+    <Hero />
+    <Product />
+    {/* <Testimonial />
+    <About /> */}
+    <Contacts />
+    <Footer />
+    </>
+  )
+}
