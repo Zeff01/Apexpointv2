7936610d0f55ef1c8b22ba071adfb459badebde9
Andre Dan Dayaganon <your-email@example.com>
2023-11-20 18:40:32 +0800

    AP-17

--- app/Pages/lubie/page.tsx
+++ app/Pages/lubie/page.tsx
@@ -14,6 +14,7 @@ export default function Page() {
 
           <Navbar/>
           <Herosec/>
+          <Products/>
           <AboutSection/>
           <StoreSection/>
           <Testimonials/>
