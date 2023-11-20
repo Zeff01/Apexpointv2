115d4825ab0dcbd14e54b8e1ec72af284f212ff9
John Russel <russelj91@gmail.com>
2023-11-20 20:47:55 +0800

    aw

---
+++ app/chlorelief/components/About/page.tsx
@@ -0,0 +1,89 @@
+import React from "react";
+import Image from "next/image";
+
+const About = () => {
+  return (
+    <div className="px-10 py-10">
+      <div className="py-5 md:py-10  flex flex-col md:flex-row justify-around  sm:flex-col ">
+        <div className="mb-5 md:mb-0 md:w-1/3 w-full text-chlorelief-secondary-gray md:text-start text-start">
+          <h2 className=" text-chlorelief-secondary-green  text-5xl md:mt-5 font-bold">
+            ABOUT THE BRAND
+          </h2>
+          <div className="">
+            <h5 className="mt-5 font-bold">CHLORELIEF</h5>
+            <p className="mt-10 text-2xl">
+              Paraben-free – paraben is not a natural product and is not
+              something you&apos;d find naturally in the body. Using skin
+              products with paraben will make skin dry, brittle, cracked, and
+              swollen. Also, paraben can cause many chemicals to enter the body
+              that should not be there.
+            </p>
+          </div>
+        </div>
+        <div>
+          <Image
+            src="/assets/about-img.png"
+            alt="Watsons"
+            width={450}
+            height={350}
+            className="rounded"
+          />
+        </div>
+      </div>
+      <div className="py-5 md:py-10  bg-chlorelief-primary-gray rounded">
+        <div className="flex flex-col md:flex-row justify-around  sm:flex-col   items-center">
+          <div>
+            <h2 className="text-2xl text-chlorelief-primary-green font-bold mb-5">
+              Services
+            </h2>
+            <Image
+              src="/assets/services-icon.png"
+              alt="Watsons"
+              width={80}
+              height={50}
+              className="rounded"
+            />
+          </div>
+          <div>
+            <h2 className="text-2xl text-chlorelief-primary-green font-bold mb-5">
+              Services
+            </h2>
+            <Image
+              src="/assets/shipping-icon.png"
+              alt="Watsons"
+              width={80}
+              height={50}
+              className="rounded"
+            />
+          </div>
+          <div>
+            <h2 className="text-2xl text-chlorelief-primary-green font-bold mb-5">
+              Return
+            </h2>
+            <Image
+              src="/assets/return-icon.png"
+              alt="Watsons"
+              width={80}
+              height={50}
+              className="rounded"
+            />
+          </div>
+          <div>
+            <h2 className="text-2xl text-chlorelief-primary-green font-bold mb-5">
+              Services
+            </h2>
+            <Image
+              src="/assets/payment-icon.png"
+              alt="Watsons"
+              width={80}
+              height={50}
+              className="rounded"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- app/chlorelief/section/LandingPage.tsx
+++ app/chlorelief/section/LandingPage.tsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import Navbar from '../components/Navbar/Navbar'
-import Hero from '../components/Hero/hero'
-import Product from '../components/Product/product'
-import Contacts from '../components/Contacts/Contacts'
-import Footer from '../components/Footer/Footer'
+import React from "react";
+import Navbar from "../components/Navbar/Navbar";
+import Hero from "../components/Hero/hero";
+import Product from "../components/Product/product";
+import About from "../components/About/page";
+import Contacts from "../components/Contacts/Contacts";
+import Footer from "../components/Footer/Footer";
 
 export default function LandingPage() {
   return (
     <>
-    <Navbar />
-    <Hero />
-    <Product />
-    {/* <Testimonial />
+      <Navbar />
+      <Hero />
+      <Product />
+      {/* <Testimonial />
     <About /> */}
-    <Contacts />
-    <Footer />
+      <About />
+      <Contacts />
+      <Footer />
     </>
-  )
+  );
 }
