6e8843a32392fe33cb5d1fe0aedebd82ff15d586
John Russel <russelj91@gmail.com>
2023-11-20 20:13:09 +0800

    7

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,13 +8,32 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      // screens: {
+      //   'sm': {'max': '640px'},    // Mobile screens
+      //   'md': {'max': '767px'},    // Tablet screens
+      //   'lg': {'max': '1023px'},   // Small desktop screens
+      //   'xl': {'max': '1279px'},   // Large desktop screens
+      // },
       colors: {
-        lubie: {},
+        lubie: {
+          "white-color": "#FFFFFF",
+          "light-blue": "#B6CBE0",
+          "navy-blue": "#1C3079",
+          "gray-white": "#F0F5F3",
+          "green-light": "#7FF0C3",
+        },
         apexpoint: {},
-        chlorelief: {},
+        chlorelief: {
+          "primary-green": "#419F59",
+          "secondary-green": "#3ADC4B",
+          "primary-red": "#514E",
+          "primary-gray": "#D9D9D9",
+          "secondary-gray": "#787878",
+        },
       },
     },
   },
+
   plugins: [],
 };
 export default config;
