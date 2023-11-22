ebc4966ce12338058594b63851bd8f13f438c887
Andre Dan Dayaganon <your-email@example.com>
2023-11-22 21:15:50 +0800

    AP-20

--- app/Pages/lubie/section/Herosec.tsx
+++ app/Pages/lubie/section/Herosec.tsx
@@ -6,7 +6,7 @@ import {Lubiehero,LubieHero2,icon1,icon2, icon3, icon4, icon5, icon6} from '@/pu
 function Herosec() {
   return (
     <>
-    <div className='h-[130vh] w-full  flex flex-col items-center  bg-lubie-light-blue md:space-y-12 space-y-6'>
+    <div className='h-[130vh] w-full  flex flex-col items-center  bg-lubie-light-blue md:space-y-12 space-y-6 font-ubuntu text-lubie-navy-blue'>
         <div className="w-full md:h-1/2 h-1/4 flex  flex-row justify-center  mt-16">
             <div className="flex flex-col w-1/2 text-center items-center justify-center  space-y-8">
                     <p>Nature & Health</p>
@@ -65,7 +65,7 @@ function Herosec() {
             <div className="w-1/2 h-full flex justify-center ">
                 <div className="flex flex-col md:w-1/2 w-full items-center space-y-6 text-center ">
                     <p>Nature & Health</p>
-                    <p className='md:text-2xl text-xl font-extrabold w-1/2'>Water-based moisturizer</p>
+                    <p className='md:text-3xl text-xl font-extrabold w-[60%]'>Water-based moisturizer</p>
                     <p>Lorem ipsum dolor sit amet Lorem ipsum dolor sit Lorem ipsum dolor sit Lorem ipsum dolor sit amet Lorem ipsum dolor sit Lorem ipsum dolor sitametamet</p>
                     <Button/>
                 </div>
@@ -74,7 +74,7 @@ function Herosec() {
         </div>
 
     </div>
-        <div className="md:h-[10vh] h-[100vh] w-full ">
+        <div className="md:h-[0vh] h-[50vh] w-full ">
 
     </div>
    </>
