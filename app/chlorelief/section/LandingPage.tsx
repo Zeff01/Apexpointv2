dc188ae53a642ef9a4dd77b75cd9bf214ca57061
420ku <mordecai51213@gmail.com>
2023-11-23 18:36:43 +0800

    pull from AP-14

--- app/chlorelief/section/LandingPage.tsx
+++ app/chlorelief/section/LandingPage.tsx
@@ -1,23 +1,16 @@
-<<<<<<< HEAD
 //@react-client
-=======
->>>>>>> 44596cebc3f75e372349bc156666c87f5ce3f2f7
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Hero from "../components/Hero/hero";
 import Product from "../components/Product/product";
-<<<<<<< HEAD
 import Testimonials from "../components/Testimonials/page";
 import About from "../components/About/page";
-=======
->>>>>>> 44596cebc3f75e372349bc156666c87f5ce3f2f7
 import Contacts from "../components/Contacts/Contacts";
 import Footer from "../components/Footer/Footer";
 
 export default function LandingPage() {
   return (
     <>
-<<<<<<< HEAD
     
     
       <Navbar />
@@ -29,15 +22,6 @@ export default function LandingPage() {
       <Contacts />
       <Footer />
    
-=======
-      <Navbar />
-      <Hero />
-      <Product />
-      {/* <Testimonial />
-    <About /> */}
-      <Contacts />
-      <Footer />
->>>>>>> 44596cebc3f75e372349bc156666c87f5ce3f2f7
     </>
   );
 }
