0a69350d6f93401a90a8a79ea039504edec53fbc
John Russel <russelj91@gmail.com>
2023-11-24 20:07:10 +0800

    w

--- app/chlorelief/components/Footer/Footer.tsx
+++ app/chlorelief/components/Footer/Footer.tsx
@@ -18,34 +18,66 @@ import i from "@/public/assets/insta.png";
 
 const companies = [
   {
+    name: "watsons",
     img: watson,
   },
+
   {
+    name: "rose",
+    img: rose,
+  },
+
+  {
+    name: "mercury",
+    img: mercury,
+  },
+
+  {
+    name: "lazada",
+    img: laz,
+  },
+
+  {
+    name: "shopee",
+    img: shop,
+  },
+];
+
+const companyMobile = [
+  {
+    name: "watsons",
     img: watsons,
   },
   {
-    img: rose,
-  },
-  {
+    name: "rose",
     img: rose1,
   },
   {
-    img: mercury,
-  },
-  {
+    name: "mercury",
     img: mercury1,
   },
   {
-    img: laz,
-  },
-  {
+    name: "lazada",
     img: laz1,
   },
   {
-    img: shop,
+    name: "shopee",
+    img: shop1,
+  },
+];
+
+const social = [
+  {
+    name: "facebook",
+    img: f,
   },
   {
-    img: shop1,
+    name: "twitter",
+    img: t,
+  },
+  {
+    name: "instagram",
+    img: i,
   },
 ];
 
@@ -61,78 +93,26 @@ const Footer = () => {
         </span>
       </div>
       <div className="stores flex flex-wrap justify-around py-5 my-5 bg-white rounded px-10 ">
-        <Image
-          src={watson}
-          alt="Watsons"
-          width={200}
-          height={80}
-          className={`hidden md:block ${styles["custom-store"]}`}
-        />
-        <Image
-          src={watsons}
-          alt="Watsons"
-          width={60}
-          height={30}
-          className={`block md:hidden ${styles["custom-store"]}`}
-        />
-
-        <Image
-          src={rose}
-          alt="Rose"
-          width={150}
-          height={150}
-          className={`hidden md:block ${styles["custom-store"]}`}
-        />
-        <Image
-          src={rose1}
-          alt="Watsons"
-          width={60}
-          height={30}
-          className={`block md:hidden ${styles["custom-store"]}`}
-        />
-        <Image
-          src={mercury}
-          alt="Mercury"
-          width={280}
-          height={80}
-          className={`hidden md:block ${styles["custom-store"]}`}
-        />
-        <Image
-          src={mercury1}
-          alt="Watsons"
-          width={60}
-          height={30}
-          className={`block md:hidden ${styles["custom-store"]}`}
-        />
-
-        <Image
-          src={laz}
-          alt="Lazada"
-          width={200}
-          height={80}
-          className={`hidden md:block ${styles["custom-store"]}`}
-        />
-        <Image
-          src={laz1}
-          alt="Watsons"
-          width={60}
-          height={30}
-          className={`block md:hidden ${styles["custom-store"]}`}
-        />
-        <Image
-          src={shop}
-          alt="Shopee"
-          width={150}
-          height={150}
-          className={`hidden md:block ${styles["custom-store"]}`}
-        />
-        <Image
-          src={shop1}
-          alt="Watsons"
-          width={60}
-          height={30}
-          className={`block md:hidden ${styles["custom-store"]}`}
-        />
+        {companies.map((d, index) => (
+          <div>
+            <Image
+              src={d.img}
+              alt={d.name}
+              width={200}
+              height={80}
+              className={`hidden md:block ${styles["custom-store"]}`}
+            />
+          </div>
+        ))}
+        {companyMobile.map((d, index) => (
+          <Image
+            src={d.img}
+            alt={d.name}
+            width={60}
+            height={30}
+            className={`block md:hidden ${styles["custom-store"]}`}
+          />
+        ))}
       </div>
       <hr className="my-5" />
       <div className="img-container py-5 md:py-10  flex flex-col md:flex-row justify-around font-bold sm:flex-col items-center ">
@@ -173,27 +153,15 @@ const Footer = () => {
           Subscribe to our Social media Accounts
         </span>
         <div className="flex justify-center mt-3 md:mt-5 gap-5">
-          <Image
-            src={f}
-            alt="Facebook"
-            width={35}
-            height={35}
-            className={`${styles["custom-icon"]}`}
-          />
-          <Image
-            src={t}
-            alt="Twitter"
-            width={35}
-            height={35}
-            className={`${styles["custom-icon"]}`}
-          />
-          <Image
-            src={i}
-            alt="Instagram"
-            width={35}
-            height={35}
-            className={`${styles["custom-icon"]}`}
-          />
+          {social.map((d, index) => (
+            <Image
+              src={d.img}
+              alt={d.name}
+              width={35}
+              height={35}
+              className={`${styles["custom-icon"]}`}
+            />
+          ))}
         </div>
       </div>
     </div>
