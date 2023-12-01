9e79f0382d46d19a1b0ef24df97385948427ebe2
hayaman3 <kevin.haya@yahoo.com>
2023-12-01 14:04:51 +0800

    ui fix apexpoint contact section

--- app/apexpoint/section/contact/links/LinksCard.tsx
+++ app/apexpoint/section/contact/links/LinksCard.tsx
@@ -17,7 +17,7 @@ const ContactCards: FunctionComponent<ContactCardsProps> = ({
 	linkText,
 }) => {
 	return (
-		<div className="flex min-w-[220px] flex-col justify-center items-center gap-4 p-3 shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)] rounded-lg bg-white">
+		<div className="flex min-w-[220px] flex-col justify-center items-center gap-4 p-3 shadow-[0px_2px_4px_0px_rgba(0,0,0,0.25)] rounded-lg bg-white group">
 			{Icon}
 			<div className="flex flex-col justify-center items-center">
 				<h3 className="text-base font-medium">{title}</h3>
@@ -25,7 +25,7 @@ const ContactCards: FunctionComponent<ContactCardsProps> = ({
 			</div>
 			<a
 				href={link}
-				className=" flex items-center justify-center text-sm duration-300 ease-in hover:translate-x-1"
+				className="flex items-center justify-center text-sm duration-300 ease-in group-hover:translate-x-1 gap-1"
 				target="_blank"
 				rel="noopener noreferrer">
 				<span>{linkText}</span>
