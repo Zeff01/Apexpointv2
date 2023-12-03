f65a563347ebc31b3f52c7615199bd1bbf106183
JUSTINEYUNG02 <justineyung02@gmail.com>
2023-12-03 18:46:06 +0800

    Added this for updated hero and product section/ I change the margirn between the hero and the product section/i change the products images/ i also change the Quick Buy button color/ I also added a rating star/

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Page from "../app/Pages/lubie/page"
+import LandingPage from "./Pages/chlorelief/section/LandingPage"
 
 export default function Home() {
   return (
     <main className="">
-      <Page/>
+      <LandingPage />
+      <Page />
     </main>
   )
 }
