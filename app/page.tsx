d932e2b2dc09c34fc5ca3c3706a4962784994f73
Zeff <jzeffsomera@gmail.com>
2024-02-02 17:57:11 +0800

    test2

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,11 @@ import Page from "../app/apexpoint/page";
 export default function Home() {
   return (
     <main className="">
-      {/* SAMPLE CODE PAYMENT FEATURE */}
+      {/* SAMPLE CODE PAYMENT FEATURE  with enchancement and bugfixed*/}
+      {/* SAMPLE CODE PAYMENT FEATURE  with enchancement and bugfixed*/}
+      {/* SAMPLE CODE PAYMENT FEATURE  with enchancement and bugfixed*/}
+      {/* SAMPLE CODE PAYMENT FEATURE  with enchancement and bugfixed*/}
+      {/* SAMPLE CODE PAYMENT FEATURE  with enchancement and bugfixed*/}
       <Page />
     </main>
   );
