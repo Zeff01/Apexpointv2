4a06ce726983b71f19daecd7f8dfbe518ad1e88c
Zeff <jzeffsomera@gmail.com>
2024-02-02 17:49:20 +0800

    implemented payment feature

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Page from "../app/apexpoint/page";
 export default function Home() {
   return (
     <main className="">
+      {/* SAMPLE CODE PAYMENT FEATURE */}
       <Page />
     </main>
   );
