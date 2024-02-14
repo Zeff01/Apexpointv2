1620feeb607e05b380ef9f85d20c982475c6f736
shanepai-14 <shaneenriquez50@gmail.com>
2024-02-15 01:30:30 +0800

    UPDATE PRODUCT SECTION

--- app/chlorelief/section/Product/data.tsx
+++ app/chlorelief/section/Product/data.tsx
@@ -16,47 +16,34 @@ const ProductComponent: React.FC<ProductProps> = ({
   Price,
 }: ProductProps) => {
   return (
-    <main className='flex flex-col gap-2 min-h-screen-sm h-full'>
-    <div className=' mt-4 rounded-lg shadow-md sm:py-2 px-5 bg-chlorelief-springwood md:px-4 md:py-4 lg:px-auto lg:bg-gray-100 lg:py-16'>
-      <img className='w-18 h-24 px-12 object-contain sm:h-48 sm:w-full sm:px-0' src={ImageUrl} alt="Product Image" />
+    <main className='flex flex-col gap-2  h-full'>
+    <div className=' mt-4 rounded-lg shadow-md py-3  px-0 flex justify-center bg-chlorelief-springwood md:px-4 md:py-4 lg:px-auto lg:py-10'>
+      <img className='h-48  lg:h-64 w-18  object-contain sm:w-full sm:px-0' src={ImageUrl} alt="Product Image" />
     </div>
-    <div className='px-6 py-2 flex gap-4 rounded-lg bg-gray-100 text-gray-600 shadow-md sm:flex'>
-      <div className='text-[8px] sm:text-[12px]'>
+    <div className='px-3 sm:px-6 py-2 flex gap-4 justify-between rounded-lg bg-gray-100 text-gray-600 shadow-md sm:flex'>
+      <div className='text-sm'>
         <h3>{ProductDesc1}</h3>
         <h3>{ProductDesc2}</h3>
-        <p className='text-[10px] text-center text-green-400 sm:text-3xl'>{Rating}</p>
+        <p className='text-center text-green-400 text-3xl'>{Rating}</p>
       </div>
       <div>
-        <p className='text-[10px] w-8 font-semibold text-black sm:text-3xl sm:w-24 '>{Price}</p>
+        <p className='font-semibold text-black text-3xl sm:w-24 text-end'>{Price}</p>
       </div>
     </div>
   </main>
   
   );
 };
-
+//☆☆☆☆ star backup
 const ProductList: React.FC = () => {
   // Example data for four products
   const productsData: ProductProps[] = [
-    {
-      ImageUrl: '/assets/calamine-60ml.png',
-      ProductDesc1: 'ChloRelief Anti Itch And ',
-      ProductDesc2: 'Anti Rash Lotion 60ml',
-      Rating: '★★☆☆☆☆',
-      Price: 'P 210',
-    },
-    {
-      ImageUrl: '/assets/calamine-120ml.png',
-      ProductDesc1: 'Anti Itch And',
-      ProductDesc2: 'Anti Rash Lotion 120ml',
-      Rating: '★★☆☆☆☆',
-      Price: 'P 420',
-    },
+
     {
       ImageUrl: '/assets/antiseptic-150ml.png',
       ProductDesc1: 'ChloRelief Anti Itch And',
       ProductDesc2: 'Antiseptic Wash 60ml',
-      Rating: '★★☆☆☆☆',
+      Rating: '★★★★★★',
       Price: 'P 59',
     },
     {
@@ -66,10 +53,25 @@ const ProductList: React.FC = () => {
       Rating: '★★★★★★',
       Price: 'P 99',
     },
+    {
+      ImageUrl: '/assets/calamine-60ml.png',
+      ProductDesc1: 'ChloRelief Anti Itch And ',
+      ProductDesc2: 'Anti Rash Lotion 60ml',
+      Rating: '★★★★★★',
+      Price: 'P 210',
+    },
+    {
+      ImageUrl: '/assets/calamine-120ml.png',
+      ProductDesc1: 'ChloRelief Anti Itch And',
+      ProductDesc2: 'Anti Rash Lotion 120ml',
+      Rating: '★★★★★★',
+      Price: 'P 420',
+    },
+ 
   ];
 
   return (
-    <div className="grid gap-4 place-items-center grid-cols-2 sm:grid-cols-2 sm:px-4 md:grid-cols-2 md:px-20 lg:grid-cols-2 lg:gap-auto xl:grid-cols-4">
+    <div className="grid gap-4 place-items-center grid-cols-1 sm:grid-cols-2 sm:px-4 md:grid-cols-2 md:px-20 lg:grid-cols-2 lg:gap-auto xl:grid-cols-4">
       {productsData.map((product, index) => (
         <ProductComponent key={index} {...product} />
       ))}
--- app/chlorelief/section/Product/product.tsx
+++ app/chlorelief/section/Product/product.tsx
@@ -11,8 +11,8 @@ export default function Product() {
   return (
     <>
       <div id="product" className="flex flex-col items-center min-h-screen h-full font-intra my-10">
-        <h1 className="text-red-500 font-bold text-center text-Header-Mobile lg:text-header-Default xl:text-Header-Desktop my-10" > OUR PRODUCTS</h1>
-        <div className="flex flex-col text-center text-gray-400 text-Body-Default max-w-screen-sm mx-20 sm:text-Body-Desktop ">
+        <h1 className="text-red-500 font-bold text-center text-Header-Mobile lg:text-header-Default xl:text-Header-Desktop my-10" >OUR PRODUCTS</h1>
+        <div className="flex flex-col text-center text-gray-400 text-Body-Default max-w-screen-lg mb-4 mx-20 lg:mx-0 sm:text-Body-Desktop ">
           <p>Paraben-free paraben is not a natural product and is not something you'd
           find naturally in the body. Using skin products with paraben will make skin
           dry, brittle, cracked, and swollen. Also, paraben can cause many chemicals to 
