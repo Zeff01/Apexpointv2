9d1ca470cb4b151cc0b2289636594c989ee7efe5
shanepai-14 <shaneenriquez50@gmail.com>
2024-02-18 23:33:03 +0800

    update

--- app/lubie/section/Hero/Herosec.tsx
+++ app/lubie/section/Hero/Herosec.tsx
@@ -17,9 +17,8 @@ import {
   containerAnimation,
   itemAnimation,
 } from "../../_modules/animation/hero/animation";
-import  Lubiehero  from "@/public/assets/lubie-products/lubiehero3.png";
-import Lubie from "@/public/assets/lubie-assets/sample.png";
-
+import Lubiehero from "@/public/assets/lubie-products/lubiehero3.png";
+import Lubie from "@/public/assets/lubie-products/aboutlubie.png";
 
 function Herosec() {
   return (
@@ -41,12 +40,12 @@ function Herosec() {
               animate="visible"
             >
               <p className=" text-3xl mb-5 md:text-4xl lg:text-5xl lg:leading-[65px] font-bold text-center sm:text-start sm:w-full">
-                The first 
+                The first
                 <span className="text-lubie-neon-green text-extra-bold">
-                  &nbsp; Multi-purpose &nbsp;   
+                  &nbsp; Multi-purpose &nbsp;
                 </span>
                 <br></br>
-                FDA registered Lube <br></br> in the  Philippines
+                FDA registered Lube <br></br> in the Philippines
               </p>
             </motion.div>
             <motion.div
@@ -54,7 +53,7 @@ function Herosec() {
               initial="hidden"
               animate="visible"
             >
-              <Button title="Read More" variant="hero"/>
+              <Button title="Read More" variant="hero" />
             </motion.div>
           </div>
           <div className="opacity-20 md:opacity-100 md:flex w-full sm:w-auto   justify-start w-100 absolute top-6  md:relative sm:p-0">
@@ -67,7 +66,6 @@ function Herosec() {
                 <Image
                   src={Lubiehero}
                   alt="Lubiehero"
-                 
                   className=" object-contain z-0 lg:w-[500px] sm:w-[300px] w-[270px] m-auto"
                 />
               </motion.div>
@@ -117,19 +115,19 @@ function Herosec() {
           />
         </div>
         <div className="w-full flex flex-col mx-auto items-center justify-center  text-lubie-dark-blue ">
-          <p>Nature & Health</p>
-          <h2 className="md:text-3xl text-xl font-extrabold md:w-[auto] text-center">
-            <span className="text-lubie-navy-blue">Water-based</span>{" "}
+          <p className="mb-6 font-semibold">Nature & Health</p>
+          <h2 className="text-4xl  font-extrabold md:w-[auto] text-center mb-6">
+            <span className="text-lubie-navy-blue">Water-based</span> <br></br>
             moisturizer
           </h2>
-          <div className="md:mx-auto ">
-            <p className="md:text-left md:text-xl text-center px-5">
+          <div className="md:mx-auto mb-12">
+            <p className="md:text-left md:text-xl text-center px-5 lg:w-[600px]">
               Lorem ipsum dolor sit amet Lorem ipsum dolor sit Lorem ipsum dolor
               sit Lorem ipsum dolor sit amet Lorem ipsum dolor sit Lorem ipsum
               dolor sitametamet
             </p>
           </div>
-          <Button title="Read More" variant="about"/>
+          <Button title="Read More" variant="about" />
         </div>
       </div>
     </div>
