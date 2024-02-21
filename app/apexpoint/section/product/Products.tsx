28226a4bbefe7941ad4951cece96787427b9790f
shanepai-14 <shaneenriquez50@gmail.com>
2024-02-21 13:43:15 +0800

    hotfix-apex products sec

--- app/apexpoint/section/product/Products.tsx
+++ app/apexpoint/section/product/Products.tsx
@@ -1,24 +1,19 @@
-import {
-  ProductData,
-  productData,
-} from "@/components/data/apexpoint/productData";
-import Section from "@/components/ui/Section";
-import Details from "./components/Details/Details";
-import Card from "@/components/ui/Card";
-import Header from "@/components/ui/Header";
+import { ProductData, productData } from '@/components/data/apexpoint/productData';
+import Section from '@/components/ui/Section';
+import Details from './components/Details/Details';
+import Card from '@/components/ui/Card';
+import Header from '@/components/ui/Header';
 
 export const Products = () => {
   return (
     <Section id="products" className="flex flex-col gap-4 min-[425px]:gap-10 ">
       <Header>Our Products</Header>
-      <div className="w-full flex flex-col min-[425px]:items-center md:items-start justify-center md:flex-col gap-y-10  ">
+      <div className="w-full flex flex-col  items-center  justify-center md:flex-col gap-y-10  ">
         {productData.map((item: ProductData) => {
           return (
             <Card
-              className={`h-full w-full  flex ${
-                item.name === "chlorelief"
-                  ? "flex-col md:flex-row"
-                  : "flex-col md:flex-row-reverse"
+              className={`h-full w-full max-w-[1000px] flex ${
+                item.name === 'chlorelief' ? 'flex-col md:flex-row' : 'flex-col md:flex-row-reverse'
               } gap-6 mb-10`}
               key={item.name}
             >
--- app/apexpoint/section/product/components/Details/Details.tsx
+++ app/apexpoint/section/product/components/Details/Details.tsx
@@ -1,27 +1,19 @@
-import { ProductData } from "@/components/data/apexpoint/productData";
-import React from "react";
-import Logo from "./components/Logo";
-import Title from "./components/Title";
-import Description from "./components/Description";
-import List from "./components/List";
-import Button from "@/components/ui/Button";
-import Slider from "./components/Slider";
-import Carousel from "./components/Carousel";
+import { ProductData } from '@/components/data/apexpoint/productData';
+import React from 'react';
+import Logo from './components/Logo';
+import Title from './components/Title';
+import Description from './components/Description';
+import List from './components/List';
+import Button from '@/components/ui/Button';
+import Slider from './components/Slider';
+import Carousel from './components/Carousel';
 
-const Details = ({
-  name,
-  title,
-  description,
-  img,
-  logo,
-  url,
-  benefits,
-}: ProductData) => {
+const Details = ({ name, title, description, img, logo, url, benefits }: ProductData) => {
   return (
     <>
       {/* <Slider name={name} logo={logo} /> */}
       <Carousel name={name} logo={logo} url={url} />
-      <div className="w-full md:w-1/2 px-0 md:px-[4%] flex flex-col gap-1 min-[425px]:gap-2 lg:gap-3  ">
+      <div className="w-full md:w-1/2 px-0 md:px-[4%] flex justify-center flex-col gap-1 min-[425px]:gap-2 lg:gap-3  ">
         <Logo className="object-fit" img={img} name={name} />
         <Title title={title} />
         <Description description={description} />
