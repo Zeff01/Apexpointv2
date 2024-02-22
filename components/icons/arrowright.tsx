8cf69e395c564a1b5b3745bb8755cc1e24daf466
morimc <mori.mcatherine@gmail.com>
2024-02-23 06:05:11 +0900

    updated apexpoint product links

---
+++ components/icons/arrowright.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const RightArrowIcon = () => {
+  return (
+    <svg
+      className="w-2 h-2"
+      aria-hidden="true"
+      xmlns="http://www.w3.org/2000/svg"
+      fill="currentColor"
+      viewBox="0 0 10 16"
+    >
+      <path d="M3.414 1A2 2 0 0 0 0 2.414v11.172A2 2 0 0 0 3.414 15L9 9.414a2 2 0 0 0 0-2.828L3.414 1Z" />
+    </svg>
+  );
+};
+
+export default RightArrowIcon;
