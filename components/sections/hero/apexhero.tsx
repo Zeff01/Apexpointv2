68ad1ab7eea61bf377e3d4ab9ccabafddfe6ec73
Shane F. Enriquez <96838519+shanepai-14@users.noreply.github.com>
2024-02-27 01:01:26 +0800

    update

--- components/sections/hero/apexhero.tsx
+++ components/sections/hero/apexhero.tsx
@@ -4,10 +4,10 @@ import ApexSwiper from "@/components/carousel/ApexHeroCarousel"
 import Header from '@/app/apexpoint/section/hero/components/Header';
 const apexhero = () => {
   return (
-    <section className="min-h-screen bg-apexpoint-light-orange flex justify-center items-center flex-col gap-4">
+    <section className="min-h-screen bg-apexpoint-light-orange flex justify-center items-center flex-col gap-8 md:gap-4">
         <Header />
        <ApexSwiper />
-      <p className="w-[600px] text-center text-xl">
+      <p className="md:w-[600px] text-center text-xl">
       ApexPoint Pharmaceutical and Medical Supplies Manufacturing OPC. Providing superior products with the use of <span className="text-chlorelief-soft-green">sustainable</span> raw materials.
       </p>
       
