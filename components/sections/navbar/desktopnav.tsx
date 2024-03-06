45ee7552feb94df4de7ff9602ff584d6d3e68d16
morimc <mori.mcatherine@gmail.com>
2024-03-07 00:42:09 +0900

    updated for bg music

--- components/sections/navbar/desktopnav.tsx
+++ components/sections/navbar/desktopnav.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 import Dropdown from '@/components/sections/navbar/dropdown';
+import BackgroundMusic from '@/components/sections/navbar/bgmusic';
 
 // image
 import apexLogo from '@/public/assets/apex-assets/apexpoint-name.png';
@@ -30,17 +31,23 @@ const DesktopNav: React.FC<DesktopNavProps> = ({ title }) => {
   return (
     <nav className="font-share font-bold shadow-lg md:shadow-md flex w-full justify-between items-center sm:px-12 px-3 py-4">
       <div className="flex items-center gap-2">
-       
-          <div className=" md:flex">
-            <Link href="/">
-              <Image
-                className={`h-8 w-auto filter ${title === 'Apexpoint' ? "" : 'invert'} hover:opacity-80 hover:scale-105 duration-200`}
-                src={apexLogo}
-                alt="apexpoint"
-              />
-            </Link>
-          </div>
-      
+        <div className="hidden md:flex">
+          <Link href="/">
+            <Image
+              className={`h-8 w-auto filter ${title === 'Apexpoint' ? '' : 'invert'} hover:opacity-80 hover:scale-105 duration-200`}
+              src={apexLogo}
+              alt="apexpoint"
+            />
+          </Link>
+        </div>
+        <div className=" md:hidden">
+          <Image
+            className={`h-8 w-auto filter ${title === 'Apexpoint' ? '' : 'invert'} hover:opacity-80 hover:scale-105 duration-200`}
+            src={apexLogo}
+            alt="apexpoint"
+          />
+        </div>
+
         {title === 'Apexpoint' ? (
           <div className="h-9 hidden md:flex border-black border-l-2 pl-2 flex-col justify-center">
             <Link className="hover:opacity-80 hover:scale-105 duration-200 " href="/chlorelief">
@@ -56,7 +63,7 @@ const DesktopNav: React.FC<DesktopNavProps> = ({ title }) => {
           </div>
         )}
       </div>
-      <ul className="hidden md:flex gap-5">
+      <ul className="hidden md:inline-flex gap-5">
         {navLinks.map((link) =>
           link.label === 'Products' && title === 'Apexpoint' ? (
             <Dropdown />
@@ -69,6 +76,9 @@ const DesktopNav: React.FC<DesktopNavProps> = ({ title }) => {
           ),
         )}
       </ul>
+      <div className="absolute left-3 top-15">
+        <BackgroundMusic />
+      </div>
     </nav>
   );
 };
