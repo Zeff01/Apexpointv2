0b635bc7f214134e3e8001708ed9cf49148c8711
morimc <mori.mcatherine@gmail.com>
2026-04-21 01:12:28 +0900

    feat: updated photo and logo, add links for learn more and read more, removed youtube and linkedin, added tiktok link

--- components/data/apexpoint/socialsData.tsx
+++ components/data/apexpoint/socialsData.tsx
@@ -1,6 +1,6 @@
-import { FaFacebook, FaInstagram, FaLinkedin, FaYoutube } from "react-icons/fa";
-import { FaXTwitter } from "react-icons/fa6";
-const IconClassName = "hover:scale-110 hover:text-black";
+import { FaFacebook, FaInstagram, FaTiktok } from 'react-icons/fa';
+import { FaXTwitter } from 'react-icons/fa6';
+const IconClassName = 'hover:scale-110 hover:text-black';
 
 export type SocialsData = {
   icon: JSX.Element;
@@ -10,22 +10,18 @@ export type SocialsData = {
 export const socialsData: SocialsData[] = [
   {
     icon: <FaFacebook size={23} className={IconClassName} />,
-    link: "https://www.facebook.com/chlorelief",
+    link: 'https://www.facebook.com/chlorelief',
   },
   {
     icon: <FaInstagram size={23} className={IconClassName} />,
-    link: "https://www.instagram.com/chlorelief/?img_index=1",
+    link: 'https://www.instagram.com/chlorelief/?img_index=1',
   },
   {
     icon: <FaXTwitter size={23} className={IconClassName} />,
-    link: "https://twitter.com/chlorelief",
+    link: 'https://twitter.com/chlorelief',
   },
   {
-    icon: <FaLinkedin size={23} className={IconClassName} />,
-    link: "https://www.linkedin.com/in/apexpoint-pharmaceutical-and-medical-supplies-manufacturing-a66524202/",
-  },
-  {
-    icon: <FaYoutube size={23} className={IconClassName} />,
-    link: "https://www.youtube.com/",
+    icon: <FaTiktok size={23} className={IconClassName} />,
+    link: 'https://www.tiktok.com/@lubie.chlorelief?_r=1&_t=ZS-95gc1Xsd6uW',
   },
 ];
